9938ddf2d816a5e5b9684381a45bff5921776001
rahmartins01 <rayssamartins1256@gmail.com>
2025-11-25 09:00:00 -0400

    comit-parar-o-erro

--- src/index.ts
+++ src/index.ts
@@ -2,35 +2,33 @@ import Receita, { ReceitaDoce } from "./Receita";
 import Categoria from "./Categoria";
 
 try {
-    const sobremesas = new Categoria(1, "Sobremesas");
-    const massas = new Categoria(2, "Massas");
+  const sobremesas = new Categoria(1, "Sobremesas");
+  const massas = new Categoria(2, "Massas");
 
-    const macarrao = new Receita(
-        1,
-        "Macarrão",
-        "Um bom almoço.",
-        "Coloque o macarrão no fogo e cozinhe por 20 minutos."
-    );
+  const macarrao = new Receita(
+    1,
+    "Macarrão",
+    "Um bom almoço.",
+    "Coloque o macarrão no fogo e cozinhe por 20 minutos."
+  );
 
-    massas.adicionarReceita(macarrao);
+  massas.adicionarReceita(macarrao);
 
-    const boloChocolate = new ReceitaDoce(
-        2,
-        "Bolo de Chocolate",
-        "Bolo fofinho e delicioso.",
-        "Misture tudo e asse por 40 minutos.",
-        "Alto"
-    );
+  const boloChocolate = new ReceitaDoce(
+    2,
+    "Bolo de Chocolate",
+    "Bolo fofinho e delicioso.",
+    "Misture tudo e asse por 40 minutos.",
+    "Alto"
+  );
 
-    sobremesas.adicionarReceita(boloChocolate);
+  sobremesas.adicionarReceita(boloChocolate);
 
-    massas.exibirReceitas();
-    sobremesas.exibirReceitas();
+  massas.exibirReceitas();
+  sobremesas.exibirReceitas();
 
- sobremesas.adicionarReceita(boloChocolate);
-
-
+  //sobremesas.adicionarReceita(boloChocolate);
 } catch (erro: any) {
-    console.error("\n ERRO CAPTURADO:");
-    console.error(erro.message);
+  console.error("\n ERRO CAPTURADO:");
+  console.error(erro.message);
 }
